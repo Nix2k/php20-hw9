e47d4f09647e3731711c6a0e068300f444e4465f
Nix2k <mihail.bokov@gmail.com>
2018-03-03 10:38:46 +0300

    v3.0

--- classes.php
+++ classes.php
@@ -1,115 +1,115 @@
 <?php
 class Car
 {
-	private $vendor;
-	private $model;
-	private $color;
-	private $velocity = 0;
-	private $acceleration = 0;
-	
-	public function __construct($vendor, $model, $color)
-	{
-		$this->vendor = $vendor;
-		$this->model = $model;
-		$this->color = $color;
-	}
-	
-	public function startEngine()
-	{
-		echo 'Engine strarted';	
-	}
-	
-	public function stopEngine() 
-	{
-		echo 'Engine stoped';	
-	}	
+    private $vendor;
+    private $model;
+    private $color;
+    private $velocity = 0;
+    private $acceleration = 0;
+    
+    public function __construct($vendor, $model, $color)
+    {
+        $this->vendor = $vendor;
+        $this->model = $model;
+        $this->color = $color;
+    }
+    
+    public function startEngine()
+    {
+        echo 'Engine strarted'; 
+    }
+    
+    public function stopEngine() 
+    {
+        echo 'Engine stoped';   
+    }   
 }
 
 class Tv
 {
-	private $vendor;
-	private $model;
-	private $screenSize;
-	private $channel = 0;
-	
-	public function __construct($vendor, $model, $screenSize)
-	{
-		$this->vendor = $vendor;
-		$this->model = $model;
-		$this->screenSize = $screenSize;
-	}
-	
-	public function nextChannel()
-	{
-		$this->channel++;
-	}
-	
-	public function prevChannel()
-	{
-		$this->channel--;
-	}
+    private $vendor;
+    private $model;
+    private $screenSize;
+    private $channel = 0;
+    
+    public function __construct($vendor, $model, $screenSize)
+    {
+        $this->vendor = $vendor;
+        $this->model = $model;
+        $this->screenSize = $screenSize;
+    }
+    
+    public function nextChannel()
+    {
+        $this->channel++;
+    }
+    
+    public function prevChannel()
+    {
+        $this->channel--;
+    }
 }
 
 class Pen
 {
-	private $color;
-	private $price;
-	
-	public function __construct($color, $price)
-	{
-		$this->color = $color;
-		$this->price = $price;
-	}
-	
-	public function getPrice()
-	{
-		return $this->price;
-	}
+    private $color;
+    private $price;
+    
+    public function __construct($color, $price)
+    {
+        $this->color = $color;
+        $this->price = $price;
+    }
+    
+    public function getPrice()
+    {
+        return $this->price;
+    }
 }
 
 class Duck
 {
-	private $weight;
-	private $sex;
-	
-	public function __construct($weight, $sex)
-	{
-		$this->weight = $weight;
-		$this->sex = $sex;
-	}
-	
-	public function sayQuack()
-	{
-		echo 'Quack';
-	}
-	
-	public function takeoff()
-	{
-		echo "I'm flying";
-	}
-	
-	public function landing()
-	{
-		echo "I'm not flying";
-	}
+    private $weight;
+    private $sex;
+    
+    public function __construct($weight, $sex)
+    {
+        $this->weight = $weight;
+        $this->sex = $sex;
+    }
+    
+    public function sayQuack()
+    {
+        echo 'Quack';
+    }
+    
+    public function takeoff()
+    {
+        echo "I'm flying";
+    }
+    
+    public function landing()
+    {
+        echo "I'm not flying";
+    }
 }
 
 class Goods
 {
-	private $vendor;
-	private $name;
-	private $price;
-	
-	public function __construct($vendor, $name, $price)
-	{
-		$this->vendor = $vendor;
-		$this->name = $name;
-		$this->price = $price;
-	}
-	
-	public function getPrice()
-	{
-		return $this->price;
-	}	
+    private $vendor;
+    private $name;
+    private $price;
+    
+    public function __construct($vendor, $name, $price)
+    {
+        $this->vendor = $vendor;
+        $this->name = $name;
+        $this->price = $price;
+    }
+    
+    public function getPrice()
+    {
+        return $this->price;
+    }   
 }
 ?>
--- news.php
+++ news.php
@@ -1,52 +1,52 @@
 <?php
-	class News
-	{
-		private $title;
-		private $text;
-		private $date;
-		private $img = null;
+    class News
+    {
+        private $title;
+        private $text;
+        private $date;
+        private $img = null;
 
-		public function __construct($title, $text, $date, $img = null)
-		{
-			$this->title = $title;
-			$this->text = $text;
-			$this->date = $date;
-			$this->img = $img;
-		}
+        public function __construct($title, $text, $date, $img = null)
+        {
+            $this->title = $title;
+            $this->text = $text;
+            $this->date = $date;
+            $this->img = $img;
+        }
 
-		public function getTitle()
-		{
-			return $this->title;
-		}
+        public function getTitle()
+        {
+            return $this->title;
+        }
 
-		public function getText()
-		{
-			return $this->text;
-		}
+        public function getText()
+        {
+            return $this->text;
+        }
 
-		public function getDate()
-		{
-			return $this->date;
-		}
+        public function getDate()
+        {
+            return $this->date;
+        }
 
-		public function getImg()
-		{
-			return $this->img;
-		}
+        public function getImg()
+        {
+            return $this->img;
+        }
 
-		public function printThis()
-		{
-			echo '<article>';
-			echo "<h4>$this->title</h4>";
-			echo "<p><small><i>$this->date</i></small></p>";
-			echo '<div class="news-body">';
-			if (!empty($this->img)) {
-				echo "<img src='./img/$this->img' alt='$this->title'>";
-			}
-			echo "<div class='news-text'>$this->text</div>";
-			echo '</div>';
-			echo '</article>';
-		}
+        public function printThis()
+        {
+            echo '<article>';
+            echo "<h4>$this->title</h4>";
+            echo "<p><small><i>$this->date</i></small></p>";
+            echo '<div class="news-body">';
+            if (!empty($this->img)) {
+                echo "<img src='./img/$this->img' alt='$this->title'>";
+            }
+            echo "<div class='news-text'>$this->text</div>";
+            echo '</div>';
+            echo '</article>';
+        }
 
-	}
+    }
 ?>
